cbddbf82e8457a8038eba3b8d397c3f81bd2523d
Marta Gaona Jiménez <mninau@gmail.com>
2016-11-16 21:21:14 +0100

    using ngModel

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,4 @@
-import {
-  Component
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 export class Hero {
   id: number;
@@ -10,19 +8,20 @@ export class Hero {
 @Component({
   selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input value="{{hero.name}}" placeholder="name">
-  </div>
-  `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  }
+  };
 }
